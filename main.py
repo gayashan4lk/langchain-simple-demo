f387c317ad01370c9f420ec04a847bcb29095c54
gayashan4lk <gayashan4lk@gmail.com>
2025-11-15 22:55:24 +0530

    refactor: update agent to use ChatAnthropic and enhance weather query functionality

--- main.py
+++ main.py
@@ -18,6 +18,15 @@ print(openai_api_key)
 print(anthropic_api_key)
 
 from langchain.agents import create_agent
+from langchain_anthropic import ChatAnthropic
+
+claude_haiku = ChatAnthropic(
+	model="claude-haiku-4-5-20251001",
+	temperature=0.5,
+	max_tokens=8192,
+	timeout=None,
+	max_retries=2,
+)
 
 def get_weather(city: str) -> str:
 	"""Get weather for a given city."""
@@ -25,14 +34,14 @@ def get_weather(city: str) -> str:
 
 def get_air_quality(city: str) -> str:
 	"""Get air quality for a given city."""
-	return f"It's always better in {city}"
+	return f"It's always windy in {city}"
 
 agent = create_agent(
-	model="openai:gpt-5-mini",
-	tools=[get_air_quality],
-	system_prompt="You are a helpful assistant. Never answer questions about the weather.",
+	model=claude_haiku,
+	tools=[get_weather, get_air_quality],
+	system_prompt="You are a helpful assistant.",
 )
 
 agent.invoke(
-	{"messages": [{"role": "user", "content": "what is the air quality in San Francisco?"}]}
+	{"messages": [{"role": "user", "content": "How is the general weather and air quality in San Francisco?"}]}
 )
